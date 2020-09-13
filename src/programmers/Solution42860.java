96d4ea29710850bdf5736fafbf482e2cf0d69b72
khJoo5 <delight_joo@naver.com>
2020-09-13 23:22:17 +0900

    Add programmers 42860
    탐욕법(Greedy)
    - 조이스틱

---
+++ src/programmers/Solution42860.java
@@ -0,0 +1,33 @@
+package programmers;
+import java.util.*;
+
+public class Solution42860 {	//탐욕법(Greedy) > 조이스틱
+	public static void main(String[] args) {
+		System.out.println(solution("JEROEN"));
+	}
+	public static int solution(String name) {
+		int answer = 0;
+		int min = name.length() - 1;
+        for(int i = 0 ; i < name.length() ; i++) {
+			int up = name.charAt(i) - 'A';
+			int down = 'Z' - name.charAt(i) + 1;
+			
+			if (up < down) {
+				answer += up;
+			} else {
+				answer += down;
+			}
+
+			if (name.charAt(i) != 'A') {
+				int next = i + 1;
+				while (next < name.length() && name.charAt(next) == 'A') {
+					next++;
+				}
+				int move = i*2 + name.length() - next;
+				min = Math.min(min, move);
+			}
+		}       
+        
+        return answer + min;
+    }
+}
